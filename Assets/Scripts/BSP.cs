d8862afe96c881e79a713dbf9a07f589a34d6c95
Jordan <finalzeroxx@gmail.com>
2022-01-20 17:37:00 +0100

    Begin Room Generation + Fix rooms size

--- Assets/Scripts/BSP.cs
+++ Assets/Scripts/BSP.cs
@@ -1,16 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class BSP : MonoBehaviour
 {
+    [Header("Tiles")]
     [SerializeField] private TileBase cell;
+    [SerializeField] private TileBase wallCell;
     [SerializeField] private TileBase roomCell;
 
-    [SerializeField] private int iterations;
+    [Header("Settings")]
+    [Range(1, 4)][SerializeField] private int iterations;
+    [Range(0, 1)] [SerializeField] private float minSlice, maxSlice;
 
     private Tilemap tilemap;
 
     private readonly Room map = new Room(0, 50, 0, 50);
+
+    private float chanceModifier = 0.5f;
+
+    private List<Room> rooms = new List<Room>();
 
     private void Awake()
     {
@@ -25,10 +34,10 @@ public class BSP : MonoBehaviour
         {
             for (var j = 0; j < map.MaxY; j++)
             {
-                if(i == 0) tilemap.SetTile(new Vector3Int(i, j, 0), roomCell);
-                else if(j == 0) tilemap.SetTile(new Vector3Int(i, j, 0), roomCell);
-                else if (i == map.MaxX - 1) tilemap.SetTile(new Vector3Int(i, j, 0), roomCell);
-                else if (j == map.MaxY - 1) tilemap.SetTile(new Vector3Int(i, j, 0), roomCell);
+                if(i == 0) tilemap.SetTile(new Vector3Int(i, j, 0), wallCell);
+                else if(j == 0) tilemap.SetTile(new Vector3Int(i, j, 0), wallCell);
+                else if (i == map.MaxX - 1) tilemap.SetTile(new Vector3Int(i, j, 0), wallCell);
+                else if (j == map.MaxY - 1) tilemap.SetTile(new Vector3Int(i, j, 0), wallCell);
                 else tilemap.SetTile(new Vector3Int(i, j, 0), cell);
             }
         }
@@ -36,29 +45,58 @@ public class BSP : MonoBehaviour
         Split(iterations, map);
     }
 
-
+    private void GenerateRooms()
+    {
+        foreach (var room in rooms)
+        {
+            var roomStartX = room.MinX + Random.Range(1, room.MaxX / 4);
+            var roomEndX = room.MaxX - Random.Range(1, room.MaxX / 4);
+            
+            var roomStartY = room.MinY + Random.Range(1, room.MaxY / 4);
+            var roomEndY = room.MaxY - Random.Range(1, room.MaxY / 4);
+            
+            for (var i = roomStartX; i < roomEndX; i++)
+            {
+                for (var j = roomStartY; j < roomEndY; j++)
+                {
+                    tilemap.SetTile(new Vector3Int(i, j, 0), roomCell);
+                }
+            }
+        }
+    }
+    
     private void Split(int nbIterations, Room room)
     {
-        if(nbIterations == 0) return;
-
-        var rooms = SplitRooms(room.MinX, room.MaxX, room.MinY, room.MaxY);
+        if (nbIterations == 0) return;
         
-        Split(nbIterations - 1, rooms[0]);
-        Split(nbIterations - 1, rooms[1]);
+        var splitsRooms = SplitRooms(room.MinX, room.MaxX, room.MinY, room.MaxY);
+
+        if (nbIterations == 1)
+        {
+            rooms.Add(splitsRooms[0]);
+            rooms.Add(splitsRooms[1]);
+        }
+        
+        Split(nbIterations - 1, splitsRooms[0]);
+        Split(nbIterations - 1, splitsRooms[1]);
     }
     
     private Room[] SplitRooms(int minX, int maxX, int minY, int maxY)
     {
-        var axe = Random.Range(0, 2);
-
-        if (axe == 0)
+        if (Random.Range(0f, 1f) > chanceModifier)
         {
-            //var height = maxY - minY;
-            var cellRandomCoordinate = Random.Range(minY + 1, maxY);
-                    
+            chanceModifier += 0.1f;
+            
+            var height = maxY - minY;
+            
+            var min = (int)(height * minSlice) + minY;
+            var max = (int)(height * maxSlice) + minY;
+            
+            var cellRandomCoordinate = Random.Range(min, max);
+
             for (var x = minX + 1; x < maxX; x++)
             {
-                tilemap.SetTile(new Vector3Int(x, cellRandomCoordinate, 0), roomCell);
+                tilemap.SetTile(new Vector3Int(x, cellRandomCoordinate, 0), wallCell);
             }
 
             var room1 = new Room(minX, maxX, minY, cellRandomCoordinate);
@@ -68,16 +106,23 @@ public class BSP : MonoBehaviour
         }
         else
         {
-            var cellRandomCoordinate = Random.Range(minX + 1, maxX);
-                    
+            chanceModifier -= 0.1f;
+            
+            var width = maxX - minX;
+            
+            var min = (int)(width * minSlice) + minX;
+            var max = (int)(width * maxSlice) + minX;
+            
+            var cellRandomCoordinate = Random.Range(min, max);
+
             for (var y = minY + 1; y < maxY; y++)
             {
-                tilemap.SetTile(new Vector3Int(cellRandomCoordinate, y, 0), roomCell);
+                tilemap.SetTile(new Vector3Int(cellRandomCoordinate, y, 0), wallCell);
             }
             
             var room1 = new Room(minX, cellRandomCoordinate, minY, maxY);
             var room2 = new Room(cellRandomCoordinate, maxX, minY, maxY);
-            
+
             return new[] {room1, room2};
         }
     }
@@ -85,6 +130,7 @@ public class BSP : MonoBehaviour
     public void Generate()
     {
         tilemap.ClearAllTiles();
+        rooms.Clear();
         InitializeCells();
     }
 }
